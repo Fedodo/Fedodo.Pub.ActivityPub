e3e1a897ec8efed4583f462b240e4d1ac7ee3245
Lukas Nagel <lukas@lna-dev.net>
2023-06-19 06:56:53 +0200

    ✨ Added exports

--- lib/activitypub.dart
+++ lib/activitypub.dart
@@ -7,6 +7,24 @@ export 'config.dart';
 
 export 'package:activitypub/APIs/activity_api.dart';
 export 'package:activitypub/APIs/actor_api.dart';
-export 'package:activitypub/APIs/auth_base_api.dart';
+export 'package:activitypub/APIs/followers_api.dart';
+export 'package:activitypub/APIs/followings_api.dart';
+export 'package:activitypub/APIs/inbox_api.dart';
+export 'package:activitypub/APIs/likes_api.dart';
+export 'package:activitypub/APIs/outbox_api.dart';
+export 'package:activitypub/APIs/post_api.dart';
+export 'package:activitypub/APIs/shares_api.dart';
 
 export 'package:activitypub/Models/ObjectTypes/document.dart';
+export 'package:activitypub/Models/activity.dart';
+export 'package:activitypub/Models/actor.dart';
+export 'package:activitypub/Models/collection.dart';
+export 'package:activitypub/Models/collection_page.dart';
+export 'package:activitypub/Models/endpoints.dart';
+export 'package:activitypub/Models/link.dart';
+export 'package:activitypub/Models/media.dart';
+export 'package:activitypub/Models/ordered_collection.dart';
+export 'package:activitypub/Models/ordered_collection_page.dart';
+export 'package:activitypub/Models/ordered_paged_collection.dart';
+export 'package:activitypub/Models/post.dart';
+export 'package:activitypub/Models/public_key.dart';
